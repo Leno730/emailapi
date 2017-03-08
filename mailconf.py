cffa0f76d1b953d7f9219f2e485cf1110cd3d6ca
Lu.Liu <350830398@qq.com>
2017-03-08 17:26:07 +0800

    Create mailconf.py

---
+++ mailconf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# =============================================================================
+#     FileName: mailconf.py
+#         Desc: 2017-03-06 15:06
+#       Author: Lu.liu
+#        Email: liulou730@163.com
+#     HomePage:
+#     Function: Email server config file
+# =============================================================================
+SERVER = "xxx.xxxx.com"
+PORT = xxx
+mailUser = "xxx@example.com"
+password = "xxxx"
